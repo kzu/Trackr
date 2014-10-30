8440b9374a155a635f37ee8be05cafada137dc90
Daniel Cazzulino <daniel@cazzulino.com>
2014-10-30 01:56:06 -0300

    Make the tracker even more generic and general purpose
    Support arbitrary GA Measurement Protocol parameters via Url query string.
    Support default values for parameters via app settings.

--- README.md
+++ README.md
@@ -5,15 +5,33 @@ A Google Analytics tracking forwarder
 
 ## Problem
 
-Say you have a simple static site in Amazon S3, a GitHub wiki, a static podcast feed, etc., and you just want to track page views / downloads of specific assets. It may be that you don't control the client where the requests are made (i.e. a feed reader or some external app that consumes those assets). In those cases, you need a server-side solution that can call Google Analytics with the tracking information, prior to letting users download (typically via a redirect). 
+Say you have a simple static site in Amazon S3, a GitHub wiki, a static podcast feed, etc., and you just want to 
+track page views / downloads of specific assets. It may be that you don't control the client where the requests 
+are made (i.e. a feed reader or some external app that consumes those assets). In those cases, you need a server-side 
+solution that can call Google Analytics with the tracking information, prior to letting users download (typically 
+via a redirect). 
 
 Now you have to read about how to do it (not that it's too complicated, as you can see in this implementation ;)).
 
 ## Solution
 
-You just fork this repo, publish it to Azure (or whatever app platform that runs bare-bones ASP.NET 4.0 or later) and provide just two bits of configuration: TrackingID and RedirectTo. And presto! Any URL that you send to this new website will push that pageview to Google Analytics with the given tracking ID, passing on the client IP and user agent, and ultimately redirect to same file but on the RedirectTo domain.
+You just fork this repo, publish it to Azure (or whatever app platform that runs bare-bones ASP.NET 4.0 or later) and 
+optionally provide any of the supported [Google Analytics Measurement Protocol parameters](https://developers.google.com/analytics/devguides/collection/protocol/v1/parameters) 
+via app settings. These parameters via settings can be overriden on a per-request/url basis, so they are essentially 
+your defaults. 
 
-In Azure, the two configuration values can be provided via the web site configuration "blade" (panel in a fancy word).
+Make sure you provide one way (app settings) or the other (url query string) all the [required parameters](https://developers.google.com/analytics/devguides/collection/protocol/v1/reference#required).
+
+> Note: [Client ID or cid](https://developers.google.com/analytics/devguides/collection/protocol/v1/parameters#cid) 
+> is treated specially. If not received as a url query string parameter, a new Guid will be generated on the handler 
+> and it will be set as a cookie named "cid" for reuse in future requests.
+ 
+The domain you want to redirect to can be specified one way (app settings) or the other (url query string) with the 
+[Document Host Name or dh](https://developers.google.com/analytics/devguides/collection/protocol/v1/parameters#dh) 
+parameter.
+
+And that's it! Any URL that you send to this new website will push that pageview to Google Analytics with the given 
+parameters, passing on the client IP and user agent, and ultimately redirect to same file but on the "dh" domain.
 
 > Note: it only does GET redirects.
 
--- TrackrHandler.cs
+++ TrackrHandler.cs
@@ -1,40 +1,79 @@
 ﻿using System;
+using System.Collections.Specialized;
 using System.Configuration;
+using System.Diagnostics;
 using System.IO;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web;
 
-public class TrackHandler : IHttpHandler
+public class TrackrHandler : IHttpHandler
 {
-	private static string TrackingID = ConfigurationManager.AppSettings["TrackingID"];
-	private static string BaseUrl = ConfigurationManager.AppSettings["RedirectTo"];
-
 	public bool IsReusable { get { return true; } }
 
 	public void ProcessRequest (HttpContext context)
 	{
+		ProcessRequest (context, true);
+	}
+
+	void ProcessRequest (HttpContext context, bool redirect)
+	{
+		// Use whatever was configured as a default.
+		var parameters = new NameValueCollection (ConfigurationManager.AppSettings);
+		foreach (var key in context.Request.QueryString.AllKeys) {
+			// Override with query string parameters.
+			parameters[key] = context.Request.QueryString[key];
+		}
+
+		// See https://developers.google.com/analytics/devguides/collection/protocol/v1/parameters#cid
+		if (string.IsNullOrEmpty (parameters["cid"])) {
+			var cookie = context.Request.Cookies.Get ("cid");
+			if (cookie == null) {
+				cookie = new HttpCookie ("cid", Guid.NewGuid ().ToString ());
+				context.Response.Cookies.Set (cookie);
+			}
+			parameters["cid"] = cookie.Value;
+		}
+
+		parameters["dp"] = context.Request.Path;
+
+		// Generate the body from parameters
+		var content = string.Join (Environment.NewLine + "&",
+			parameters.AllKeys.Select (key => key + "=" + HttpUtility.UrlEncode(parameters[key])));
+
+		Trace.WriteLine (content);
+
 		var client = new HttpClient ();
-		var redirectUrl = new Uri (new Uri (BaseUrl), context.Request.Url.PathAndQuery);
+		var response = client.PostAsync ("http://www.google-analytics.com/collect", new StringContent (content)).Result;
+		if (response.StatusCode != HttpStatusCode.OK) {
+			context.Response.StatusCode = (int)response.StatusCode;
+			context.Response.Status = context.Response.StatusDescription = response.ReasonPhrase;
+			context.Response.End ();
+			return;
+		}
 
-		var content = @"v=1
-&tid=" + TrackingID + @"
-&cid=555
-&t=pageview
-&dh=" + redirectUrl.Host + @"
-&dp=" + context.Request.Path + @"
-&dt=" + context.Request.QueryString["t"] ?? context.Request.QueryString["title"] ?? Path.GetFileName(context.Request.Url.AbsolutePath);
+		if (!redirect)
+			return;
 
-		var clientIp = IPAddress.Parse (context.Request.UserHostAddress);
-		if (!IPAddress.IsLoopback (clientIp))
-			content += @"
-&uip=" + context.Request.UserHostAddress;
+		// See https://developers.google.com/analytics/devguides/collection/protocol/v1/parameters#dl
+		var dl = parameters["dl"];
+		var dh = parameters["dh"];
+		if (!string.IsNullOrEmpty (dl)) {
+			context.Response.Redirect (dl, true);
+		} else if (!string.IsNullOrEmpty (dh)) {
+			context.Response.Redirect (new Uri ("http://" + dh + context.Request.Url.PathAndQuery).ToString (), true);
+		}
+	}
 
-		if (!string.IsNullOrEmpty (context.Request.UserAgent))
-			content += @"
-&ua=" + context.Request.UserAgent;
+	public void Track ()
+	{
+		var output = new StringWriter();
+		var context = new HttpContext(new HttpRequest("bar.png", "http://vsgallery.azurewebsites.net/feed.atom", "v=1&tid=UA-56247715-1&t=pageview&an=Visual Studio&dh=gallery.mobileessentials.org"), new HttpResponse(output));
 
-		client.PostAsync ("http://www.google-analytics.com/collect", new StringContent (content));
-		context.Response.Redirect (redirectUrl.ToString ());
+		ProcessRequest (context, false);
+
+		Console.WriteLine (((HttpStatusCode)context.Response.StatusCode).ToString());
+		Console.WriteLine (output.ToString());
 	}
 }
